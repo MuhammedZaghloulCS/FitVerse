62a929fcb657a7df7e8063b87e7c77046b1eba40
EngMahmoudAdel <mahmoud.adel.fci@gmail.com>
2025-10-21 19:19:35 +0300

    Adding admin page for testing before identity and roles

---
+++ FitVerse.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FitVerse.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AdminDashboard()
+        {
+            return View();
+        }
+    }
+}
